1ed0bfbb29690b867581e3daec29a63c93da0dd1
keyFrameM <marlowe.animation@gmail.com>
2015-11-20 05:34:21 -0600

    declaring variables and setting up inital method for add

--- app/src/main/java/com/codechris/calculator/MainActivity.java
+++ app/src/main/java/com/codechris/calculator/MainActivity.java
@@ -4,13 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+    private Button btnAdd;
+    private Button btnSubtract;
+    private Button btnMultiply;
+    private Button btnDivide;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        btnAdd =  (Button) findViewById(R.id.btnAdd);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnMultiply =  (Button) findViewById(R.id.btnMultiply);
+        btnDivide =  (Button) findViewById(R.id.btnDivide);
+        txtResult =  (TextView) findViewById(R.id.txtResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double oper1 = Double.parseDouble(operand1.getText().toString());
+                double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = oper1 + oper2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+
     }
 
     @Override
